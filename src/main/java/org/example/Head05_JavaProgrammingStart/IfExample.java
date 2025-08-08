b774ae0e7ce8655ce780cdf57bf7602a46725486
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-08 14:33:38 +0900

    update

---
+++ src/main/java/org/example/Head05_JavaProgrammingStart/IfExample.java
@@ -0,0 +1,15 @@
+package org.example.Head05_JavaProgrammingStart;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int score =92;
+
+        if(score>=90){
+            System.out.println("A학점");
+        } else if (score >=80) {
+            System.out.println("B학점");
+        }else{
+            System.out.println("C학점 이하");
+        }
+    }
+}
---
+++ src/main/java/org/example/Head06_JavaProgrammingStart/SwitchExample.java
@@ -0,0 +1,20 @@
+package org.example.Head06_JavaProgrammingStart;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("월요일");
+                break;
+            case 2:
+                System.out.println("화요일");
+                break;
+            case 3:
+                System.out.println("수요일");
+                break;
+            default:
+                System.out.println("기타 요일");
+        }
+    }
+}
