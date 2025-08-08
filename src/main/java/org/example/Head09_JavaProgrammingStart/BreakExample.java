c73e0658b9f9a48691b4325498e93c07a9247246
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-08 15:15:43 +0900

    update

---
+++ src/main/java/org/example/Head09_JavaProgrammingStart/BreakExample.java
@@ -0,0 +1,10 @@
+package org.example.Head09_JavaProgrammingStart;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            if( i== 5)break;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/org/example/Head10_JavaProgrammingStart/BreakContinueExample.java
@@ -0,0 +1,11 @@
+package org.example.Head10_JavaProgrammingStart;
+
+public class BreakContinueExample {
+    public static void main(String[] args) {
+        for(int i=1; i<=5 ;i++){
+            if(1==3)continue;
+            if(i==4)break;
+            System.out.println(i);
+        }
+    }
+}
