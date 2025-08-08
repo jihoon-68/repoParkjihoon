5c141817cc632911c5c1e084032f36093ce2c6b1
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-08 14:37:05 +0900

    update

---
+++ src/main/java/org/example/Head07_JavaProgrammingStart/ForLoopExample.java
@@ -0,0 +1,15 @@
+package org.example.Head07_JavaProgrammingStart;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for(int i= 0; i < 5; i++){
+            System.out.println("i = " + i);
+        }
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.println("i="+i+", j = " + j);
+            }
+        }
+    }
+}
