d1c4620fbe17d6697f67b89f7562f781872314a0
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-13 14:56:36 +0900

    5일차 enum 사용

---
+++ src/main/java/org/example/Head04_JavaProgrammingStart/example06/PlanetMain.java
@@ -0,0 +1,9 @@
+package org.example.Head04_JavaProgrammingStart.example06;
+
+public class PlanetMain {
+    public static void main(String[] args) {
+        for(Planet p:Planet.values()){
+            System.out.println(p);
+        }
+    }
+}
---
+++ src/main/java/org/example/Head04_JavaProgrammingStart/example07/PaymentMethod.java
@@ -0,0 +1,21 @@
+package org.example.Head04_JavaProgrammingStart.example07;
+
+public enum PaymentMethod {
+    CREDIT_CARD("신용카드"),
+    ACCOUNT_TRANSFER("계좌이체"),
+    MOBILE_PAYMENT("모바일 결재");
+
+    private final String displayName;
+
+    PaymentMethod(String displayName) {
+        this.displayName = displayName;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return "결재 방식: "+displayName;
+    }
+}
