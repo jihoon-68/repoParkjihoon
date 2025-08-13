cb2899656b7fb37a98a2f19395ab5bf8eda26f60
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-13 16:04:18 +0900

    5일차 예외처리

---
+++ src/main/java/org/example/Head04_JavaProgrammingStart/example08/NullPointerExceptionEx.java
@@ -0,0 +1,20 @@
+package org.example.Head04_JavaProgrammingStart.example08;
+
+public class NullPointerExceptionEx {
+    /**
+    static void main() {
+        printStr(null);
+    }
+    public static void printStr(String str) {
+        try {
+            System.out.println(str.toUpperCase());
+        } catch (NullPointerException e) {
+            System.out.println("예외 발생: str이 null입니다.");
+            System.out.println(e.getMessage());
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("finally 블록 실행됨");
+        }
+    }
+     **/
+}
