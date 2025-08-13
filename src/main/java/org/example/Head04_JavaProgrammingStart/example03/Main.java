d6e16a3b12462eaa88c727fa1daa88f9058433ae
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-13 14:06:36 +0900

    5일차 정적 중첩 클래스

---
+++ src/main/java/org/example/Head04_JavaProgrammingStart/example03/Main.java
@@ -0,0 +1,8 @@
+package org.example.Head04_JavaProgrammingStart.example03;
+
+public class Main {
+    public static void main(String[] args) {
+        Outer.StaticNested nested = new Outer.StaticNested();
+        nested.printValues();
+    }
+}
