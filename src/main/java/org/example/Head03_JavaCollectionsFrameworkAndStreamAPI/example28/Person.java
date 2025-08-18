f528c4cc6a168864232791e9ccb66a9837e0077c
jihoon-68 <pkrwlgns64@gmail.com>
2025-08-18 19:13:00 +0900

    6일차 [실습] Custom Comparator 작성 예시

---
+++ src/main/java/org/example/Head03_JavaCollectionsFrameworkAndStreamAPI/example28/Person.java
@@ -0,0 +1,22 @@
+package org.example.Head03_JavaCollectionsFrameworkAndStreamAPI.example28;
+
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString(){
+        return "Person{name=\'"+this.name+"\', age="+this.age+ "}";
+    }
+}
---
+++ src/main/java/org/example/Head03_JavaCollectionsFrameworkAndStreamAPI/example28/PersonAgeComparator.java
@@ -0,0 +1,11 @@
+package org.example.Head03_JavaCollectionsFrameworkAndStreamAPI.example28;
+
+import java.util.Comparator;
+
+public class PersonAgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+
+}
